068e0fccf10745091cd40191b5f5b419a6e74a21
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-10-10 17:27:35 -0400

    - add 2level

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -1,5 +1,5 @@
 #include "predictor.h"
-
+#include "string.h"
 /////////////////////////////////////////////////////////////
 // 2bitsat
 /////////////////////////////////////////////////////////////
@@ -31,19 +31,76 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
 /////////////////////////////////////////////////////////////
 
 BYTE gBHT[512];
-BYTE gPHT[8];
+BYTE gPHT[64][8];
+
+int GetPHTIndex(UINT32 PC){
+    return PC & 0b111;
+}
+
+int GetBHTIndex(UINT32 PC){
+    return (PC & 0b1111111000) >> 3;
+}
+
+int GetHistoryIndex(BYTE history){
+    return history & 0b00111111;
+}
 
 void InitPredictor_2level() {
     memset(gBHT,0, BYTE_SIZE * BHT_ENTRY_COUNT);
-    memset(gPHT,0, BYTE_SIZE * PHT_ENTRY_COUNT);
+    memset(gPHT,WEAK_NOT_TAKEN, BYTE_SIZE * PHT_ENTRY_COUNT);
 }
 
 bool GetPrediction_2level(UINT32 PC) {
-    return TAKEN;
+    int pht_index = GetPHTIndex(PC);
+    int bht_index = GetBHTIndex(PC);
+    if (gPHT[GetHistoryIndex(gBHT[bht_index])][pht_index] >= WEAK_TAKEN)
+        return TAKEN;
+    else
+        return NOT_TAKEN;
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-
+    int bht_index = GetBHTIndex(PC);
+    int pht_index = GetPHTIndex(PC);
+    int current_history = gPHT[GetHistoryIndex(gBHT[bht_index])][pht_index];
+    int modified_result = STRONG_NOT_TAKEN;
+    switch(current_history){
+        case WEAK_NOT_TAKEN:{
+            if(resolveDir!=predDir) {
+                modified_result = STRONG_NOT_TAKEN;
+            }
+            else{
+                modified_result = WEAK_TAKEN;
+            }
+            break;
+        }
+        case STRONG_NOT_TAKEN:{
+            if(resolveDir == predDir){
+                modified_result = WEAK_NOT_TAKEN;
+            }
+            break;
+        }
+        case WEAK_TAKEN:{
+            if(resolveDir == predDir){
+                modified_result = STRONG_TAKEN;
+            }
+            else{
+                modified_result = WEAK_NOT_TAKEN;
+            }
+            break;
+        }
+        case STRONG_TAKEN:{
+            if(resolveDir != predDir){
+                modified_result = WEAK_NOT_TAKEN;
+            }
+            break;
+        }
+        default:
+            break;
+    }
+    gPHT[GetHistoryIndex(gBHT[bht_index])][pht_index] = modified_result;
+    gBHT[bht_index] <<= 1;
+    gBHT[bht_index] |= resolveDir;
 }
 
 /////////////////////////////////////////////////////////////
