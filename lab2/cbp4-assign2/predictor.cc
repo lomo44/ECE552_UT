98228c2b205b6d5d09e4021ba69dc37324a6ed5d
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-10-10 17:35:15 -0400

    - 2level changes

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -100,7 +100,7 @@ void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 bra
     }
     gPHT[GetHistoryIndex(gBHT[bht_index])][pht_index] = modified_result;
     gBHT[bht_index] <<= 1;
-    gBHT[bht_index] |= resolveDir;
+    gBHT[bht_index] |= (resolveDir == predDir) ? TAKEN : NOT_TAKEN;
 }
 
 /////////////////////////////////////////////////////////////
