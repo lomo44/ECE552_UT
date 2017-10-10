e5baeb4fc3d0c68a2ae0de4e378a82e49717268a
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-10-10 16:58:42 -0400

    - add common types

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -3,6 +3,15 @@
 /////////////////////////////////////////////////////////////
 // 2bitsat
 /////////////////////////////////////////////////////////////
+#define BYTE unsigned char
+#define BYTE_SIZE sizeof(BYTE)
+#define BHT_ENTRY_COUNT 512
+#define PHT_ENTRY_COUNT 8
+#define STRONG_TAKEN 3
+#define WEAK_TAKEN 2
+#define WEAK_NOT_TAKEN 1
+#define STRONG_NOT_TAKEN 0
+
 
 void InitPredictor_2bitsat() {
 
@@ -10,7 +19,7 @@ void InitPredictor_2bitsat() {
 
 bool GetPrediction_2bitsat(UINT32 PC) {
 
-  return TAKEN;
+    return TAKEN;
 }
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
@@ -21,13 +30,16 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
 // 2level
 /////////////////////////////////////////////////////////////
 
-void InitPredictor_2level() {
+BYTE gBHT[512];
+BYTE gPHT[8];
 
+void InitPredictor_2level() {
+    memset(gBHT,0, BYTE_SIZE * BHT_ENTRY_COUNT);
+    memset(gPHT,0, BYTE_SIZE * PHT_ENTRY_COUNT);
 }
 
 bool GetPrediction_2level(UINT32 PC) {
-
-  return TAKEN;
+    return TAKEN;
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
@@ -44,7 +56,7 @@ void InitPredictor_openend() {
 
 bool GetPrediction_openend(UINT32 PC) {
 
-  return TAKEN;
+    return TAKEN;
 }
 
 void UpdatePredictor_openend(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
