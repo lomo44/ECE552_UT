03750d5501ea9ba38a0f8c49292ef62fb49e41b2
Gujiang Lin <gujiang.lin@mail.utoronto.ca>
2017-10-12 15:08:08 -0400

    working version with 7.41 average

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -172,104 +172,104 @@ void UpdatePredictor_RP(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchT
 /////////////////////////////////////////////////////////////
 // openend
 /////////////////////////////////////////////////////////////
+//
+//#define PCPT_ROW 512
+//#define PCPT_COL 31
+//#define PCPT_LIMIT 73
+//
+//char pcpt_weight[PCPT_ROW][PCPT_COL];
+//bool pcpt_hst[PCPT_COL];
+//
+//void InitPredictor_PerC(){
+//    int col_cont = 0;
+//    int row_cont = 0;
+//    for (col_cont=0;col_cont<PCPT_COL; col_cont++) {
+//        pcpt_hst [col_cont]= TAKEN;
+//    }
+//    for (row_cont=0;row_cont<PCPT_ROW; row_cont++) {
+//        for (col_cont =0; col_cont<PCPT_COL; col_cont++){
+//            pcpt_weight [row_cont][col_cont]= 0;
+//        }
+//    }
+//
+//}
+//int GetPrediction_PerC(UINT32 PC) {
+//    int index = (PC>>4) % PCPT_ROW; // mod or and, need to be determine
+//    int sum = 0;
+//    for (int i=0; i < PCPT_COL; i++) {
+//        if (pcpt_hst[i] == TAKEN) {
+//            sum += pcpt_weight [index][i];
+//        } else {
+//            sum -= pcpt_weight [index][i];
+//        }
+//    }
+//
+//    return (sum >= 0) ? TAKEN : NOT_TAKEN;
+//
+//}
+//void UpdatePredictor_PerC(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
+//    int index = (PC>>4) % PCPT_ROW; // mod or and, need to be determine
+//    int col_cont =0;
+//
+//    for (col_cont =0; col_cont < PCPT_COL -1; col_cont++) {
+//        if(resolveDir == pcpt_hst[col_cont]){
+//            pcpt_weight[index][col_cont] += 1;
+//        }
+//        else{
+//            pcpt_weight[index][col_cont] += -1;
+//        }
+//    }
+//    for (col_cont =0; col_cont < PCPT_COL -1; col_cont++) {
+//        pcpt_hst[col_cont] = pcpt_hst[col_cont + 1];
+//    }
+//        pcpt_hst[PCPT_COL-1]= resolveDir;
+//}
 
-#define PCPT_ROW 512
-#define PCPT_COL 31
-#define PCPT_LIMIT 73
-
-char pcpt_weight[PCPT_ROW][PCPT_COL];
-bool pcpt_hst[PCPT_COL];
-
-void InitPredictor_PerC(){
-    int col_cont = 0;
-    int row_cont = 0;
-    for (col_cont=0;col_cont<PCPT_COL; col_cont++) {
-        pcpt_hst [col_cont]= TAKEN;
-    }
-    for (row_cont=0;row_cont<PCPT_ROW; row_cont++) {
-        for (col_cont =0; col_cont<PCPT_COL; col_cont++){
-            pcpt_weight [row_cont][col_cont]= 0;
-        }
-    }
-
-}
-int GetPrediction_PerC(UINT32 PC) {
-    int index = (PC>>4) % PCPT_ROW; // mod or and, need to be determine
-    int sum = 0;
-    for (int i=0; i < PCPT_COL; i++) {
-        if (pcpt_hst[i] == TAKEN) {
-            sum += pcpt_weight [index][i];
-        } else {
-            sum -= pcpt_weight [index][i];
-        }
-    }
-
-    return (sum >= 0) ? TAKEN : NOT_TAKEN;
-
-}
-void UpdatePredictor_PerC(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-    int index = (PC>>4) % PCPT_ROW; // mod or and, need to be determine
-    int col_cont =0;
-
-    for (col_cont =0; col_cont < PCPT_COL -1; col_cont++) {
-        if(resolveDir == pcpt_hst[col_cont]){
-            pcpt_weight[index][col_cont] += 1;
-        }
-        else{
-            pcpt_weight[index][col_cont] += -1;
-        }
-    }
-    for (col_cont =0; col_cont < PCPT_COL -1; col_cont++) {
-        pcpt_hst[col_cont] = pcpt_hst[col_cont + 1];
-    }
-        pcpt_hst[PCPT_COL-1]= resolveDir;
-}
-
-#define MAP_SIZE 1024
-#define WINDOW_SIZE 7
-#define HISTORY_SIZE 32
-int gMAP[MAP_SIZE];
-
-void InitPredicor_MAP(){
-    memset(gMAP,0,MAP_SIZE);
-}
-
-int GetIndex_MAP(UINT32 PC){
-    return PC>>4 & (MAP_SIZE-1);
-}
-
-int GetPrediction_MAP(UINT32 PC){
-    int index = GetIndex_MAP(PC);
-    short history = gMAP[index];
-    short current_pattern = history & ((1<<WINDOW_SIZE)-1);
-    int shift_count = HISTORY_SIZE-WINDOW_SIZE-1;
-    int starting_shift_count = 1;
-    int potential_result = -1;
-    int final_result = -1;
-    int history_pattern = 0;
-    while(starting_shift_count<=shift_count){
-        potential_result = history & 0b1;
-        history_pattern  = (history >> 1) & ((1<<WINDOW_SIZE)-1);
-        if(history_pattern == current_pattern){
-            final_result = potential_result;
-            break;
-        }
-        starting_shift_count++;
-        history_pattern>>=1;
-    }
-    return final_result;
-}
-
-void UpdatePredictor_MAP(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-    int index = GetIndex_MAP(PC);
-    short value = gMAP[index];
-    value <<=1;
-    value |= resolveDir==TAKEN? 1:0;
-    gMAP[index] = value;
-}
+//#define MAP_SIZE 1024
+//#define WINDOW_SIZE 7
+//#define HISTORY_SIZE 32
+//int gMAP[MAP_SIZE];
+//
+//void InitPredicor_MAP(){
+//    memset(gMAP,0,MAP_SIZE);
+//}
+//
+//int GetIndex_MAP(UINT32 PC){
+//    return PC>>4 & (MAP_SIZE-1);
+//}
+//
+//int GetPrediction_MAP(UINT32 PC){
+//    int index = GetIndex_MAP(PC);
+//    short history = gMAP[index];
+//    short current_pattern = history & ((1<<WINDOW_SIZE)-1);
+//    int shift_count = HISTORY_SIZE-WINDOW_SIZE-1;
+//    int starting_shift_count = 1;
+//    int potential_result = -1;
+//    int final_result = -1;
+//    int history_pattern = 0;
+//    while(starting_shift_count<=shift_count){
+//        potential_result = history & 0b1;
+//        history_pattern  = (history >> 1) & ((1<<WINDOW_SIZE)-1);
+//        if(history_pattern == current_pattern){
+//            final_result = potential_result;
+//            break;
+//        }
+//        starting_shift_count++;
+//        history_pattern>>=1;
+//    }
+//    return final_result;
+//}
+//
+//void UpdatePredictor_MAP(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
+//    int index = GetIndex_MAP(PC);
+//    short value = gMAP[index];
+//    value <<=1;
+//    value |= resolveDir==TAKEN? 1:0;
+//    gMAP[index] = value;
+//}
 
 
-#define SELECTION_COUNTER_SIZE 999
+#define SELECTION_COUNTER_SIZE 997
 BYTE gSELECTION_COUNTER[SELECTION_COUNTER_SIZE];
 bool gMaster_last = TAKEN;
 bool gAltpred_last = TAKEN;
