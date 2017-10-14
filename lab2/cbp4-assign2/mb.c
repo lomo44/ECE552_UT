da0d395b733e3d761a2198e76f8e128308ae6e9b
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-10-13 20:21:52 -0400

    - Add benchmark - Modified makefile

---
+++ lab2/cbp4-assign2/mb.c
@@ -0,0 +1,15 @@
+//
+// Created by lizhuan1 on 13/10/17.
+//
+
+#include <stdlib.h>
+
+int main(){
+    int a;
+    for (int i = 0 ; i < 100000; i++){
+        if( a %4 == 0){
+            a = 10;
+        }
+        a=15;
+    }
+}
