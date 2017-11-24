d9e673cf2abb78b776703bd440dd525091845e40
lomo44 <vamj44@gmail.com>
2017-11-24 10:11:32 -0500

    modified mbq6.c

--- lab4/simplesim-3.0d-assig4/benchmarks/mbq6.c
+++ lab4/simplesim-3.0d-assig4/benchmarks/mbq6.c
@@ -8,11 +8,11 @@ int main(){
     int i = 0;
     int counter = 0;
     int* array = calloc(ARRAY_SIZE,sizeof(int));
-    while(i < ARRAY_SIZE){
-        array[i] +=2;
-        i+=strides[counter%3]; // i advance in a paticular stride pattern {16,64,8}
+    while(counter < ARRAY_SIZE){
+        array[counter%3] +=2;// i advance in a paticular stride pattern {16,64,8}
         counter++;
     }
+    
     free(array);
     return 0;
 }
