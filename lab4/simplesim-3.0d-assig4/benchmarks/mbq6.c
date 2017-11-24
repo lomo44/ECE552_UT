a80d27b910fa288c944f33bf881b869cd3ccff40
lomo44 <vamj44@gmail.com>
2017-11-24 09:21:35 -0500

    - Fix the mbq6

--- lab4/simplesim-3.0d-assig4/benchmarks/mbq6.c
+++ lab4/simplesim-3.0d-assig4/benchmarks/mbq6.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define ARRAY_SIZE 1000
-#define STRIDE 2
+
 int main(){
+    int strides[3] = {16,64,8};
     int i = 0;
+    int counter = 0;
     int* array = calloc(ARRAY_SIZE,sizeof(int));
     while(i < ARRAY_SIZE){
         array[i] +=2;
-        i+=STRIDE;
+        i+=strides[counter%3]; // i advance in a paticular stride pattern {16,64,8}
+        counter++;
     }
     free(array);
     return 0;
